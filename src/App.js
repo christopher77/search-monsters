28cf765c36ec1e5a88baec434a32db0e5ce3b79e
christopher <christoph.roa@gmail.com>
2019-12-09 21:34:23 -0500

    Add search monster functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list-component";
+import { SearchBox } from "./components/search-box/search-box-component";
 
 function App() {
+  const [monsters, setMonsters] = React.useState([]);
+  const [searchField, setSearchField] = React.useState("");
+
+  function fetchingData() {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response =>
+      response.json().then(users => setMonsters(users))
+    );
+  }
+  fetchingData();
+
+  const filteredMonsters = monsters.filter(monster =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Search Monsters</h1>
+      <SearchBox
+        placeholder="search monsters"
+        handleChange={e => {
+          setSearchField(e.target.value);
+        }}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
